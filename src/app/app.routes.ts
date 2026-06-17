c9ae4143201bd04335d1489c95dab2110c8c7e22
SERGE HONDERDOS <5478388+shonderdos@users.noreply.github.com>
2026-06-17 10:04:37 +0200

    added the contact page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from "@angular/router";
 import { Experiences } from "./components/experiences/experiences";
 import { Home } from "./components/home/home";
+import { Contact } from "./components/contact/contact";
 
 export const routes: Routes = [
   { path: "", component: Home },
   { path: "experiences", component: Experiences },
+  { path: "contact", component: Contact },
 ];
