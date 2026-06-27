829a44ea275f418f57a0f47ba0f9588346f97dab
SERGE HONDERDOS <5478388+shonderdos@users.noreply.github.com>
2026-06-27 11:58:14 +0200

    add about me page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from "@angular/router";
 import { Experiences } from "./components/experiences/experiences";
 import { Home } from "./components/home/home";
 import { Contact } from "./components/contact/contact";
+import { AboutMe } from "./components/about-me/about-me";
 
 export const routes: Routes = [
   { path: "", component: Home },
   { path: "experiences", component: Experiences },
   { path: "contact", component: Contact },
+  { path: "about-me", component: AboutMe },
 ];
