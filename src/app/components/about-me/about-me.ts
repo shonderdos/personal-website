0189f50b74f4ff428654a231454e4c01772895da
SERGE HONDERDOS <5478388+shonderdos@users.noreply.github.com>
2026-06-28 20:11:53 +0200

    update courses page to add links to frontedmasters and add indicator for external links

--- src/app/components/about-me/about-me.ts
+++ src/app/components/about-me/about-me.ts
@@ -1,7 +1,9 @@
 import { Component } from "@angular/core";
+import { ExternalLinkDirective } from "../../directives/external-link.directive";
 
 @Component({
   templateUrl: "./about-me.html",
   styleUrl: "./about-me.css",
+  imports: [ExternalLinkDirective],
 })
 export class AboutMe {}
